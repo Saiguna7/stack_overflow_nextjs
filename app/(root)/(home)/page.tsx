3dbe7f4d0d29f522da85423ca5ad77686e0a9384
user.name "saiguna <user.email "saiguna.vanam67@gmail.com>
2024-08-11 21:32:27 +0530

    all added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,87 +1,89 @@
-import Questioncard from "@/components/cards/Questioncard";
+import QuestionCard from "@/components/cards/Questioncard";
 import HomeFilters from "@/components/home/HomeFilters";
-import Fliter from "@/components/shared/Fliter";
+import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
+import Pagination from "@/components/shared/Pagination";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
-import { getQuestions } from "@/lib/actions/question.action";
+import {
+  getQuestions,
+  getRecommendedQuestions,
+} from "@/lib/actions/question.action";
+import { SearchParamsProps } from "@/types";
 import Link from "next/link";
-const questions = [
-  {
-    _id: "1",
-    title: "Cascading Delets in SQLAlchemy?",
-    tag: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "i=john-doe-jpg",
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date(
-      "Sat Aug 10 2024 17:35:04 GMT+0530 (India Standard Time)"
-    ),
-  },
-  {
-    _id: "2",
-    title: "Cascading Delets in SQLAlchemy?",
-    tag: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "i=john-doe-jpg",
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date(
-      "Sat Aug 10 2024 17:35:04 GMT+0530 (India Standard Time)"
-    ),
-  },
-];
-export default async function Home() {
-  const result = await getQuestions({});
+
+import type { Metadata } from "next";
+import { auth } from "@clerk/nextjs/server";
+
+export const metadata: Metadata = {
+  title: "Home | Dev Overflow",
+};
+
+export default async function Home({ searchParams }: SearchParamsProps) {
+  const { userId } = auth();
+
+  let result;
+
+  if (searchParams?.filter === "recommended") {
+    if (userId) {
+      result = await getRecommendedQuestions({
+        userId,
+        searchQuery: searchParams.q,
+        page: searchParams.page ? +searchParams.page : 1,
+      });
+    } else {
+      result = {
+        questions: [],
+        isNext: false,
+      };
+    }
+  } else {
+    result = await getQuestions({
+      searchQuery: searchParams.q,
+      filter: searchParams.filter,
+      page: searchParams.page ? +searchParams.page : 1,
+    });
+  }
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">All Question</h1>
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
         <Link href="/ask-question" className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
             Ask a Question
           </Button>
         </Link>
       </div>
+
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchbar
           route="/"
-          iconPostion="left"
+          iconPosition="left"
           imgSrc="/assets/icons/search.svg"
           placeholder="Search for questions"
           otherClasses="flex-1"
         />
-        <Fliter
+
+        <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
           containerClasses="hidden max-md:flex"
         />
       </div>
+
       <HomeFilters />
+
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
           result.questions.map((question) => (
-            <Questioncard
+            <QuestionCard
               key={question._id}
               _id={question._id}
               title={question.title}
-              tags={question.tag}
+              tags={question.tags}
               author={question.author}
               upvotes={question.upvotes}
               views={question.views}
@@ -91,14 +93,18 @@ export default async function Home() {
           ))
         ) : (
           <NoResult
-            title="There' no question to show"
-            description="Be the first to break the silence! Ask a Question and kickstart the
-        discussion. our query could be the next big thing others learn from. Get
-        involved! 💡"
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
-            LinkTitle="Ask the question"
+            linkTitle="Ask a Question"
           />
         )}
+      </div>
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
       </div>
     </>
   );
--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -1,28 +1,65 @@
-"use client";
-import { Input } from "@/components/ui/input";
-import Image from "next/image";
-import React from "react";
+"use client"
 
-type CustomInputProps = {
-  route: string;
-  iconPostion: string;
-  imgSrc: string;
-  placeholder: string;
-  otherClasses: string;
-};
+import { Input } from '@/components/ui/input'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils'
+
+interface CustomInputProps {
+  route: string
+  iconPosition: string
+  imgSrc: string
+  placeholder: string
+  otherClasses?: string
+}
+
 const LocalSearchbar = ({
   route,
-  iconPostion,
+  iconPosition,
   imgSrc,
   placeholder,
   otherClasses,
 }: CustomInputProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get('q');
+
+  const [search, setSearch] = useState(query || '');
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if(search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'q',
+          value: search
+        })
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        console.log(route, pathname)
+        if(pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ['q']
+          })
+
+          router.push(newUrl, { scroll: false });
+        }
+
+      }
+    }, 300);
+    
+    return () => clearTimeout(delayDebounceFn)
+  }, [search, route, pathname, router, searchParams, query])
+
   return (
-    <div
-      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
-    >
-      {iconPostion === "left" && (
-        <Image
+    <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}>
+      {iconPosition === 'left' && (
+        <Image 
           src={imgSrc}
           alt="search icon"
           width={24}
@@ -30,14 +67,17 @@ const LocalSearchbar = ({
           className="cursor-pointer"
         />
       )}
+
       <Input
         type="text"
         placeholder={placeholder}
-        onChange={() => {}}
-        className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
       />
-      {iconPostion === "right" && (
-        <Image
+
+      {iconPosition === 'right' && (
+        <Image 
           src={imgSrc}
           alt="search icon"
           width={24}
@@ -46,7 +86,7 @@ const LocalSearchbar = ({
         />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default LocalSearchbar;
+export default LocalSearchbar
